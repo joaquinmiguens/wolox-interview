4b636b1c0e99abcc0f40a3cfb8376d48a591ccec
Joaquin Miguens <juacomiguens@hotmail.com.ar>
2020-09-23 11:33:35 -0300

    Se declaran los componentes y paginas agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './containers/hero/hero.component';
+import { ShowcaseComponent } from './components/showcase/showcase.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, HeroComponent, ShowcaseComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
