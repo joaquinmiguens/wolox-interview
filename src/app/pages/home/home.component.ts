b548b1181d22532b5aa76d83cfb011ab8037a01f
Joaquin Miguens <juacomiguens@hotmail.com.ar>
2020-09-24 13:51:54 -0300

    declare benefit objects array items

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { Benefit } from '../../models/benefit.model';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,56 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 })
 export class HomeComponent implements OnInit {
   public twitterIcon = faTwitter;
-  constructor() {}
+  public assets: string;
+  public benefits: Array<Benefit> = [
+    {
+      image: '',
+      title: '',
+      id: '',
+    },
+  ];
+  constructor() {
+    this.assets = '../../../assets';
+    this.benefits = [
+      {
+        image: `${this.assets}/Ic_Hour.svg`,
+        title: 'Flexibilidad Horaria',
+        id: '001',
+      },
+      {
+        image: `${this.assets}/Ic_HomeOffice.svg`,
+        title: 'Home Office',
+        id: '002',
+      },
+      {
+        image: `${this.assets}/Ic_Workshops.svg`,
+        title: 'Capacitaciones y Workshops',
+        id: '003',
+      },
+      {
+        image: `${this.assets}/Ic_DrinkSnacks.svg`,
+        title: 'Snacks, frutas y bebidas gratis',
+        id: '004',
+      },
+      {
+        image: `${this.assets}/Ic_laptop.svg`,
+        title: 'Semana Remota',
+        id: '005',
+      },
+      {
+        image: `${this.assets}/Ic_brain.svg`,
+        title: 'Trabajar en ultimas tecnologias',
+        id: '006',
+      },
+    ];
+  }
 
   ngOnInit(): void {}
 
   navigateToTwitter() {
     window.location.href = 'https://twitter.com/Wolox';
+  }
+  trackById(index: number, item: Benefit): string {
+    return item.id;
   }
 }
