952619290efc066c1a20b69e4ced07bdc2e01925
Joaquin Miguens <juacomiguens@hotmail.com.ar>
2020-09-26 15:12:37 -0300

    add httpClientModule to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomeModule } from './pages/home/home.module';
 import { LoginModule } from './pages/login/login.module';
 import { TechListModule } from './pages/tech-list/tech-list.module';
+
 import { SharedModule } from './shared/shared/shared.module';
+
 import { GuestGuard } from './guard/guest.guard';
 import { LoggedInGuard } from './guard/logged-in.guard';
+
 import { AuthService } from './service/auth.service';
 import { AppService } from './app.service';
+import { HttpClientModule } from '@angular/common/http';
 
 export function appInit(appService: AppService) {
   return () => appService.initializeApp();
@@ -24,6 +29,7 @@ export function appInit(appService: AppService) {
     LoginModule,
     TechListModule,
     SharedModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [
