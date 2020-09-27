ada1a64ac649edd76a7f4cd24909f5d3500a2fe9
Joaquin Miguens <juacomiguens@hotmail.com.ar>
2020-09-27 15:07:04 -0300

    Remove header from home view

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,19 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { ShowcaseComponent } from 'src/app/components/showcase/showcase.component';
-import { HeaderComponent } from 'src/app/components/header/header.component';
 import { HomeComponent } from './home.component';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
 import { SharedModule } from 'src/app/shared/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    ShowcaseComponent,
-    FooterComponent,
-    HomeComponent,
-  ],
-  exports: [HeaderComponent, ShowcaseComponent, FooterComponent],
+  declarations: [ShowcaseComponent, FooterComponent, HomeComponent],
+  exports: [ShowcaseComponent, FooterComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
