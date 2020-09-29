95e2ae7427a0892e44353a4dae98237afa2c73a5
Joaquin Miguens <juacomiguens@hotmail.com.ar>
2020-09-29 19:30:55 -0300

    unit testings

--- src/app/guard/guest.guard.spec.ts
+++ src/app/guard/guest.guard.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GuestGuard } from './guest.guard';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 describe('GuestGuard', () => {
   let guard: GuestGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [AuthService, Router],
+    });
     guard = TestBed.inject(GuestGuard);
   });
 
--- src/app/shared/shared/components/header/header.component.spec.ts
+++ src/app/shared/shared/components/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { AuthService } from 'src/app/service/auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,9 +10,10 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+      imports: [HttpClientModule],
+      providers: [AuthService],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
