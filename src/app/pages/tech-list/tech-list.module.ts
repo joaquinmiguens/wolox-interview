3a7e746e5ba9130c1dbfdd75a8da754528fc69a7
Joaquin Miguens <juacomiguens@hotmail.com.ar>
2020-09-29 01:43:35 -0300

    technologies count pipe and design

--- src/app/pages/tech-list/tech-list.module.ts
+++ src/app/pages/tech-list/tech-list.module.ts
@@ -5,10 +5,10 @@ import { SharedModule } from 'src/app/shared/shared/shared.module';
 import { TechListRoutingModule } from './tech-list-routing.module';
 import { TechListComponent } from './tech-list.component';
 import { ListItemComponent } from 'src/app/components/list-item/list-item.component';
-import { SearchPipe } from 'src/app/pipe/search.pipe';
+import { SearchPipe, CountPipe } from 'src/app/pipe/search.pipe';
 
 @NgModule({
-  declarations: [TechListComponent, ListItemComponent, SearchPipe],
+  declarations: [TechListComponent, ListItemComponent, SearchPipe, CountPipe],
   imports: [SharedModule, CommonModule, TechListRoutingModule],
 })
 export class TechListModule {}
--- src/app/pipe/search.pipe.ts
+++ src/app/pipe/search.pipe.ts
@@ -10,35 +10,58 @@ export class SearchPipe implements PipeTransform {
     searchText: string,
     order: string
   ): Candidate[] {
-    if (!items) {
-      return [];
-    }
-    if (!searchText) {
-      return this.sorting(items, order);
-    }
-    searchText = searchText.toLocaleLowerCase();
-    const authorArray = items.filter(($item: Candidate) => {
-      return $item.author.toLocaleLowerCase().includes(searchText);
-    });
-    const typeArray = items.filter(($item: Candidate) => {
-      return $item.type.toLocaleLowerCase().includes(searchText);
-    });
-    let newArray = authorArray.concat(typeArray);
-    newArray = [...new Set([...authorArray, ...typeArray])];
-    return this.sorting(newArray, order);
+    return merge(items, searchText, order);
   }
+}
 
-  sorting(items: Candidate[], order: string) {
-    if (order === 'Ascendente') {
-      const sortingArray = items.sort((first: Candidate, second: Candidate) =>
-        first.author < second.author ? -1 : first.author > second.author ? 1 : 0
-      );
-      return sortingArray;
+@Pipe({
+  name: 'countPipe',
+})
+export class CountPipe implements PipeTransform {
+  transform(items: Candidate[], searchText: string): string[] {
+    const techArray: string[] = [];
+    const array: Candidate[] = merge(items, searchText, 'Ascendente');
+    if (array.length === 0) {
+      return [];
     } else {
-      const sortingArray = items.sort((first: Candidate, second: Candidate) =>
-        first.author > second.author ? -1 : first.author < second.author ? 1 : 0
-      );
-      return sortingArray;
+      array.map(($candidate: Candidate) => {
+        techArray.push($candidate.tech);
+      });
+      console.log(techArray);
+      return techArray;
     }
+  }
+}
+
+export function merge(items: Candidate[], searchText: string, order: string) {
+  if (!items) {
+    return [];
+  }
+  if (!searchText) {
+    return sorting(items, order);
+  }
+  searchText = searchText.toLocaleLowerCase();
+  const authorArray = items.filter(($item: Candidate) => {
+    return $item.author.toLocaleLowerCase().includes(searchText);
+  });
+  const typeArray = items.filter(($item: Candidate) => {
+    return $item.type.toLocaleLowerCase().includes(searchText);
+  });
+  let newArray = authorArray.concat(typeArray);
+  newArray = [...new Set([...authorArray, ...typeArray])];
+  return sorting(newArray, order);
+}
+
+export function sorting(items: Candidate[], order: string) {
+  if (order === 'Ascendente') {
+    const sortingArray = items.sort((first: Candidate, second: Candidate) =>
+      first.author < second.author ? -1 : first.author > second.author ? 1 : 0
+    );
+    return sortingArray;
+  } else {
+    const sortingArray = items.sort((first: Candidate, second: Candidate) =>
+      first.author > second.author ? -1 : first.author < second.author ? 1 : 0
+    );
+    return sortingArray;
   }
 }
