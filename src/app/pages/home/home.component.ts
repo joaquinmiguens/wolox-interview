b023b05e5b2235def963e6da856e510495b052e1
Joaquin Miguens <juacomiguens@hotmail.com.ar>
2020-09-30 13:14:19 -0300

    first attemp to home Accesibility

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -37,49 +37,49 @@ export class HomeComponent implements OnInit {
       {
         image: `${this.assets}/Ic_Bullet_1.svg`,
         text:
-          'Estudiantes avanzados o recibidos de carrearas del rubro informatico, sistemas o electronicos',
+          'Estudiantes avanzados o recibidos de carrearas del rubro informatico, sistemas o electronicos.',
         id: '007',
       },
       {
         image: `${this.assets}/Ic_Bullet_2.svg`,
-        text: 'Ingles intermedio/avanzado',
+        text: 'Ingles intermedio ó avanzado.',
         id: '008',
       },
       {
         image: `${this.assets}/Ic_Bullet_3.svg`,
-        text: 'Conocimiento en metodologas agiles (deseable)',
+        text: 'Conocimiento en metodologas agiles (deseable).',
         id: '009',
       },
     ];
     this.benefits = [
       {
         image: `${this.assets}/Ic_Hour.svg`,
-        title: 'Flexibilidad Horaria',
+        title: 'Flexibilidad Horaria.',
         id: '001',
       },
       {
         image: `${this.assets}/Ic_HomeOffice.svg`,
-        title: 'Home Office',
+        title: 'Home Office.',
         id: '002',
       },
       {
         image: `${this.assets}/Ic_Workshops.svg`,
-        title: 'Capacitaciones y Workshops',
+        title: 'Capacitaciones y Workshops.',
         id: '003',
       },
       {
         image: `${this.assets}/Ic_DrinkSnacks.svg`,
-        title: 'Snacks, frutas y bebidas gratis',
+        title: 'Snacks, frutas y bebidas gratis.',
         id: '004',
       },
       {
         image: `${this.assets}/Ic_laptop.svg`,
-        title: 'Semana Remota',
+        title: 'Semana Remota.',
         id: '005',
       },
       {
         image: `${this.assets}/Ic_brain.svg`,
-        title: 'Trabajar en ultimas tecnologias',
+        title: 'Trabajar en ultimas tecnologias.',
         id: '006',
       },
     ];
